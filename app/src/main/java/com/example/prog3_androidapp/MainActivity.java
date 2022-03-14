11579f88fd899810ae78d1dbee5124e59cab8ba8
Crovvyboi <81975137+Crovvyboi@users.noreply.github.com>
2022-03-14 17:42:23 +0100

    Meal detail
    Displays some details from a selected meal

--- app/src/main/java/com/example/prog3_androidapp/MainActivity.java
+++ app/src/main/java/com/example/prog3_androidapp/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.prog3_androidapp;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final LinkedList<String> mWordList = new LinkedList<>();
+    // Change to list of meals
+    private final LinkedList<Meal> mList = new LinkedList<>();
 
     private RecyclerView mRecyclerView;
     private ListAdapter mAdapter;
@@ -21,18 +36,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Retrieve list of meals
+        // Retrieve from Room, database or json (isn't required)
         for (int i = 0; i < 20; i++) {
-            mWordList.addLast("Word " + i);
+            Meal newmeal = new Meal("Meal " + i, (double) i);
+            mList.add(newmeal);
         }
 
         // Get a handle to the RecyclerView.
         mRecyclerView = findViewById(R.id.recyclerView);
         // Create an adapter and supply the data to be displayed.
-        mAdapter = new ListAdapter(this, mWordList);
+        mAdapter = new ListAdapter(this, mList);
         // Connect the adapter with the RecyclerView.
         mRecyclerView.setAdapter(mAdapter);
         // Give the RecyclerView a default layout manager.
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("RestrictedApi")
+    public void GoToDetails(View view) {
+        // Goes to meal details on click of item in list
+
+        TextView tv = view.findViewById(R.id.word);
+        String mealString = (String) tv.getText();
+        // Selected Meal
+        Log.d(LOG_TAG, mealString);
+
+        MainActivity activity = new MainActivity();
+
+        // Transfer lists and get meal
+        ArrayList<Meal> meals = new ArrayList<>();
+        for (Meal meal: mList) {
+            meals.add(meal);
+        }
+        Meal meal = meals.stream().filter(meal1 -> meal1.GetName().equals(mealString)).findFirst().orElse(null);
+        Log.d(LOG_TAG, meal.GetPrice());
+
+        // Send object (serializable) to next activity
+        Intent newIntent = new Intent(this, MealDetails.class);
+        newIntent.putExtra("sentMeal", meal);
+        startActivity(newIntent);
     }
 }
--- app/src/main/java/com/example/prog3_androidapp/MealDetails.java
+++ app/src/main/java/com/example/prog3_androidapp/MealDetails.java
@@ -1,14 +1,53 @@
 package com.example.prog3_androidapp;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+@SuppressLint("RestrictedApi")
 public class MealDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_details);
+
+        // Get object from MainActivity
+        Intent intent = getIntent();
+        Meal meal = (Meal) intent.getSerializableExtra("sentMeal");
+
+        Log.d(LOG_TAG, "Current meal: " + meal.GetName());
+
+        SetFields(meal);
+    }
+
+    public void SetFields(Meal meal){
+        // Set fields in view
+        View mealImage = this.findViewById(R.id.MealImage);
+
+        View chefImage = this.findViewById(R.id.ChefImage);
+
+        // List propertiesList = this.findViewById(R.id.PropertiesList);
+
+
+        TextView titleText = this.findViewById(R.id.TitleText);
+        titleText.setText(meal.GetName());
+
+        TextView priceText = this.findViewById(R.id.PriceText);
+        priceText.setText(String.valueOf(meal.GetPrice()));
+
+        TextView descripText = this.findViewById(R.id.DescriptionText);
+        descripText.setText(meal.GetDescription());
+
+        TextView servedText = this.findViewById(R.id.ServedText);
+        servedText.setText(meal.GetServed());
     }
 }
